11db7fdf2ee665f0ed7ab2202f293dabb4154e5e
semila <semilaneto@gmail.com>
2024-04-22 18:57:24 +0100

    update CSRF

--- ngola/settings.py
+++ ngola/settings.py
@@ -25,12 +25,12 @@ SECRET_KEY = 'django-insecure-sw22wv6h-ctt=z%t7w#p%ogu79*$@vs&=0&t!jh1q^@ch%xr1i
 TOKEN_CSRF = os.getenv('TOKEN_CSRF')
 if TOKEN_CSRF:
     SECRET_KEY = TOKEN_CSRF
-    CSRF_TRUSTED_ORIGINS = ['https://projetongoladjango-production.up.railway.app/']
+    CSRF_TRUSTED_ORIGINS = ['https://projetongoladjango-production.up.railway.app']
 else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://projetongoladjango-production.up.railway.app/", "localhost", "http://127.0.0.1" ]
+ALLOWED_HOSTS = ["projetongoladjango-production.up.railway.app/", "localhost", "http://127.0.0.1" ]
 
 
 # Application definition
